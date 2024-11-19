157a09583ba542aaacaa3b7b14ebe61227aff745
MidStein <deepakchauhan19feb@gmail.com>
2024-11-19 12:48:03 +0530

    Add index and details views

--- tasks_app/models.py
+++ tasks_app/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=75)
     description = models.CharField(max_length=250)
+    pub_time = models.DateTimeField("time published")
+
+    def __str__(self):
+        return self.title
--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,4 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Task
 
 def index(request):
-    return HttpResponse("Hello, World! This is the tasks-app.")
+    tasks_list = Task.objects.order_by("-pub_time")
+    context = {
+        "tasks_list": tasks_list,
+    }
+    return render(request, "tasks_app/index.html", context)
+
+def details(request, id):
+    task = Task.objects.filter(id=id)
+    context = {
+        "task": task
+    }
+    return render(request, "tasks_app/details.html", context)
