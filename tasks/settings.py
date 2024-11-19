2b06894a89e5645a7cb12943c0796ec77369fc31
MidStein <deepakchauhan19feb@gmail.com>
2024-11-19 11:28:01 +0530

    Create Task model

--- tasks/settings.py
+++ tasks/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tasks_app.apps.TasksAppConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
